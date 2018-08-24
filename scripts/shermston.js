c84973491b65a391646badfc50585c2b7c5eab6f
Adam T Johnston <atomjohnston@gmail.com>
2018-08-24 00:47:39 -0400

    I think the images are close to done

--- scripts/shermston.js
+++ scripts/shermston.js
@@ -2,11 +2,8 @@
 
 var cache = {
     mediaState: $('#media-state'),
-    //mainContent: $('.main-content'),
-    separators: $('.separator-image, .scale'),
+    separators: $('.separator-image, .scale')
 }
-
-//var states = Object.freeze(['xs', 'sm', 'md', 'lg', 'xl']);
 
 var compose = function () {
     var fnClosure = arguments;
@@ -34,17 +31,6 @@ var getMediaState = function () {
     return cache.mediaState.css('--state');
 }
 
-// var adjustContentWidth = function (targetState) {
-//     var targetWidth = getWidth(targetState);
-//     if (cache.mainContent.hasClass(targetWidth))
-//         return;
-//     var removeUs = states.filter(function (st) { return st !== targetState }).map(function (st) { return getWidth(st); });
-//     for (var i = 0; i < removeUs.length; i++) {
-//         cache.mainContent.removeClass(removeUs[i]);
-//     }
-//     cache.mainContent.addClass(targetWidth);
-// }
-
 var scaleSeparators = function () {
     cache.separators.css('height', Math.floor(((window.innerWidth / window.innerHeight) * 100) + 1).toString() + '%');
 }
@@ -53,34 +39,9 @@ var getHandler = function (ms) {
     if (ms === 'sm' || ms === 'xs')
         return scaleSeparators;
     return function () { };
-    // switch (ms) {
-    //     case 'xs':
-    //     case 'sm': return compose(partial(adjustContentWidth, 'sm'), scaleSeparators);
-    //     case 'md': return partial(adjustContentWidth, 'md');
-    //     case 'lg':
-    //     case 'xl': return partial(adjustContentWidth, 'lg');
-    // }
 }
 
-// var getWidth = function (ms) {
-//     switch (ms) {
-//         case 'xs':
-//         case 'sm': return 'col';
-//         case 'md': return 'col-10'
-//         case 'lg':
-//         case 'xl': return 'col-7';
-//     }
-// }
-
 var resizeFn = getHandler(getMediaState());
-
-$('.nav-link').on('click', function (e) {
-    var target = $(e.target).attr('scroll-to');
-    $('.navbar-toggler').click();
-    if (!target)
-        return;
-    window.scrollTo(0, $(target).offset().top - (window.innerHeight / 3));
-});
 
 resizeFn();
 
