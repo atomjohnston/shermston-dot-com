e3ae6610e1592055f709237b07c1a3831d9e07cf
Adam T Johnston <atomjohnston@gmail.com>
2018-09-04 22:58:56 -0400

    basic rest functionality works

--- rsvp.py
+++ rsvp.py
@@ -1,37 +1,61 @@
 import base64
 import json
+from collections import namedtuple
 
 import redis
 from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest, Forbidden
+
 
 app = Flask(__name__)
 rc = redis.StrictRedis()
+Guest = namedtuple('Guest', 'count name')
 
 
 @app.route('/guest-count', methods=['GET', 'POST'])
 def get_count():
-    try:
-        usr, passwd = get_basic(request.headers.get('Authorization').lower())
-        if request.method == 'GET':
-            guest = json.loads(rc.hget(usr, passwd))
-            return jsonify(guest_count=guest.count)
-    except Exception as e:
-        pass
+    guest_name, invite = get_basic(request.headers.get('Authorization'))
+    success, guest = retrieve_guest(guest_name, invite)
+    if not success:
+        raise Forbidden()
+
+    if request.method == 'GET':
+        return jsonify(guest_count=guest.count)
+
+    if request.method == 'POST':
+        update = request.get_json()
+        print(update)
+        u_guest = guest._replace(count=update['count'])
+        rc.hset(guest_name.lower(), invite, json.dumps(u_guest._asdict()))
+        return jsonify(guest_count=u_guest.count)
 
 
 def get_basic(auth_header):
-    if not auth_header[:13] == 'authorization':
-        raise Exception(400)
-    return pipe(
-        auth_header,
-        lambda s: s.split(' ').pop(),
-        base64.decodebytes,
-        lambda b: b.decode('utf-8'),
-        lambda s: s.split(':'),
-    )
+    if not auth_header:
+        raise BadRequest('missing Authorization header')
+    return decode_auth(auth_header)
 
 
-def pipe(data, *funcs):
-    for func in funcs:
-        data = func(data)
-    return data
+def retrieve_guest(name, invite):
+    raw = rc.hget(name.lower(), invite)
+    print(raw)
+    if not raw:
+        return False, None
+    return True, Guest(**json.loads(raw.decode('utf-8')))
+
+
+def pipeline(*funcs):
+    def _pl(x):
+        for func in funcs:
+            x = func(x)
+        return x
+    return _pl
+
+
+decode_auth = pipeline(
+    lambda s: s.split(' ').pop().encode('ascii'),
+    base64.decodebytes,
+    lambda b: b.decode('utf-8'),
+    lambda s: s.split(':'),
+    tuple,
+)
