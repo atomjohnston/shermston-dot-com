60f89e1717caa9038aed38a9615fb89b63258e29
Adam T Johnston <atomjohnston@gmail.com>
2018-09-05 02:16:36 -0400

    another pass at invite data structure; error enum;

--- rsvp.py
+++ rsvp.py
@@ -1,33 +1,47 @@
 import base64
 import json
+from enum import Enum
 from collections import namedtuple
 
 import redis
 from flask import Flask, request, jsonify
-from werkzeug.exceptions import BadRequest, Forbidden
+from werkzeug.exceptions import BadRequest, Forbidden, Unauthorized
 
+Guest = namedtuple('Guest', 'actual invite names max surname')
 
 app = Flask(__name__)
-rc = redis.StrictRedis()
-Guest = namedtuple('Guest', 'count name')
+rc = redis.StrictRedis(encoding='utf-8')
+
+
+class InviteStatus(Enum):
+    OKAY = 1
+    WRONG_NAME = 2
+    WRONG_INVITE = 3
 
 
 @app.route('/guest-count', methods=['GET', 'POST'])
-def get_count():
+def guest_count():
     guest_name, invite = get_basic(request.headers.get('Authorization'))
-    success, guest = retrieve_guest(guest_name, invite)
-    if not success:
+    i_status, guest = retrieve_guest(invite, guest_name)
+
+    if i_status == InviteStatus.WRONG_INVITE:
+        raise Unauthorized()
+
+    if i_status == InviteStatus.WRONG_NAME:
         raise Forbidden()
 
     if request.method == 'GET':
-        return jsonify(guest_count=guest.count)
+        return jsonify(guest_count=guest.actual)
 
     if request.method == 'POST':
         update = request.get_json()
-        print(update)
-        u_guest = guest._replace(count=update['count'])
-        rc.hset(guest_name.lower(), invite, json.dumps(u_guest._asdict()))
-        return jsonify(guest_count=u_guest.count)
+        u_guest = guest._replace(actual=(update['count'] if update['count'] < guest.max else
+                                         guest.max))
+        pipe = rc.pipeline()
+        pipe.hset(invite, u_guest.surname, json.dumps(u_guest._asdict()))
+        pipe.hset(invite, 'attending', u_guest.actual)
+        pipe.execute()
+        return jsonify(guest_count=u_guest.actual)
 
 
 def get_basic(auth_header):
@@ -36,12 +50,17 @@ def get_basic(auth_header):
     return decode_auth(auth_header)
 
 
-def retrieve_guest(name, invite):
-    raw = rc.hget(name.lower(), invite)
-    print(raw)
-    if not raw:
-        return False, None
-    return True, Guest(**json.loads(raw.decode('utf-8')))
+def retrieve_guest(invite, name):
+    raw = rc.hgetall(invite)
+    if len(raw) == 0:
+        return InviteStatus.WRONG_INVITE, None
+    try:
+        return (
+            InviteStatus.OKAY,
+            Guest(**(json.loads(raw[name.lower().encode('utf-8')].decode('utf-8'))))
+        )
+    except KeyError:
+        return InviteStatus.WRONG_NAME, None
 
 
 def pipeline(*funcs):
