04aa71ead09593a423b9ab9abdcad1a491422d51
Adam T Johnston <atomjohnston@gmail.com>
2018-09-07 22:20:50 -0400

    hey webapp kinda works!

--- rsvp.py
+++ rsvp.py
@@ -2,15 +2,22 @@ import base64
 import json
 from enum import Enum
 from collections import namedtuple
+from textwrap import dedent
+from uuid import uuid4
 
 import redis
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from werkzeug.exceptions import BadRequest, Forbidden, Unauthorized
 
 Guest = namedtuple('Guest', 'actual invite names max surname')
 
 app = Flask(__name__)
 rc = redis.StrictRedis(encoding='utf-8')
+
+session2guest = rc.register_script(dedent("""\
+    local s = redis.call('get', KEYS[1])
+    local x = cjson.decode(s)
+    return redis.call('hget', x.invite, x.name)"""))
 
 
 class InviteStatus(Enum):
@@ -19,34 +26,57 @@ class InviteStatus(Enum):
     WRONG_INVITE = 3
 
 
-@app.route('/guest-count', methods=['GET', 'POST'])
+@app.route('/guest-count', methods=['GET', 'POST', 'OPTIONS'])
 def guest_count():
-    guest_name, invite = get_basic(request.headers.get('Authorization'))
-    i_status, guest = retrieve_guest(invite, guest_name)
-
-    if i_status == InviteStatus.WRONG_INVITE:
-        raise Unauthorized()
-
-    if i_status == InviteStatus.WRONG_NAME:
-        raise Forbidden()
+    if request.method == 'OPTIONS':
+        return Response(headers={
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization, Session-Id',
+        })
 
     if request.method == 'GET':
-        return jsonify(guest_count=guest.actual)
+        guest_name, invite = get_basic(request.headers.get('Authorization'))
+        i_status, guest = retrieve_guest(invite, guest_name)
+
+        if i_status == InviteStatus.WRONG_INVITE:
+            raise Unauthorized()
+
+        if i_status == InviteStatus.WRONG_NAME:
+            raise Forbidden()
+
+        session = uuid4()
+        pipe = rc.pipeline()
+        pipe.set(session, json.dumps({'name': guest_name, 'invite': invite}))
+        pipe.expire(session, 3600)
+        pipe.execute()
+        response = jsonify(
+            guest_count=guest.actual,
+            invite_count=guest.max,
+            surname=guest.surname[0].upper() + guest.surname[1:].lower(),
+            session_id=session)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
 
     if request.method == 'POST':
+        guest = Guest(**(json.loads(session2guest(keys=[request.headers.get('Session-Id')]).decode('utf-8'))))
         update = request.get_json()
         u_guest = guest._replace(actual=(update['count'] if update['count'] < guest.max else
                                          guest.max))
         pipe = rc.pipeline()
-        pipe.hset(invite, u_guest.surname, json.dumps(u_guest._asdict()))
-        pipe.hset(invite, 'attending', u_guest.actual)
+        pipe.hset(guest.invite, u_guest.surname, json.dumps(u_guest._asdict()))
+        pipe.hset(guest.invite, 'attending', u_guest.actual)
         pipe.execute()
-        return jsonify(guest_count=u_guest.actual)
+        response = jsonify(guest_count=u_guest.actual)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    
+    raise BadRequest("yeah, I can't work with that")
 
 
 def get_basic(auth_header):
     if not auth_header:
         raise BadRequest('missing Authorization header')
+    print(auth_header)
     return decode_auth(auth_header)
 
 
--- static/scripts/rsvp.js
+++ static/scripts/rsvp.js
@@ -1,8 +1,26 @@
 "use strict";
 
-var displayUpdate = function () {
+URL = 'http://127.0.0.1:5000/guest-count';
+
+var displayUpdate = function (response) {
     $('#update').removeClass('d-none');
     $('#start').addClass('d-none');
+    var guests = $('#guests');
+    guests.parent().find('label').text(response.surname + ' Guests:');
+    var menu = '';
+    for (var i = 0; i <= response.invite_count; i++) {
+        menu += i == response.guest_count 
+            ? '<option value="_" selected="selected">_</option>'.replace(/_/g, i)
+            : '<option value="_">_</option>'.replace(/_/g, i);
+    }
+    guests.html(menu);
+}
+
+var displayRsvp = function (response) {
+    $('#update').addClass('d-none');
+    $('#start').addClass('d-none');
+    $('#rsvp-count').html(response.guest_count);
+    $('#thank-you').removeClass('d-none');
 }
 
 var displayStart = function () {
@@ -10,25 +28,30 @@ var displayStart = function () {
     $('#start').removeClass('d-none');
 }
 
-var getResponseAction = function (req) {
-    switch (req.status) {
-        case 200: return displayUpdate;
-        default: throw 'unknown response ' + statusCode;
-    }
-}
-
 $('#submit-code').on('click', function() {
-    displayUpdate();
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'guest-count');
-    // xhr.setRequestHeader('Authorization', 'Basic: ' + window.btoa($('#surname').text() + ':' + $('#secret').text()));
-    // xhr.onload = function() {
-    //     var action = getResponseAction(xhr);
-    //     action();
-    // }
-    // xhr.send();
+    var auth = window.btoa($('#surname').val() + ':' + $('#secret').val());
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', URL);
+    xhr.setRequestHeader('Authorization', 'Basic: ' + auth);
+    xhr.onload = function() {
+        if (xhr.status != 200)
+            throw 'unknown response ' + xhr.status
+        var response = JSON.parse(xhr.responseText)
+        sessionStorage.setItem('shermstonSession', response.session_id);
+        displayUpdate(response);
+    }
+    xhr.send();
 });
 
 $('#update-guests').on('click', function () {
-    displayStart();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', URL);
+    xhr.setRequestHeader('Session-Id', sessionStorage.getItem('shermstonSession'));
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status != 200)
+            throw 'unknown response ' + xhr.status
+        displayRsvp(JSON.parse(xhr.responseText));
+    }
+    xhr.send(JSON.stringify({count: parseInt($('#guests').val())}));
 });
